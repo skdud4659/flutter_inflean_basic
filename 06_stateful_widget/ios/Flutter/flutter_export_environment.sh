dadbf2895e69eb505cba72c7df23b6b9118fcc6a
skdud4659 <83805167+skdud4659@users.noreply.github.com>
2023-02-04 15:43:42 +0900

    섹션6. statefulWidget

---
+++ 06_stateful_widget/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/nayeongkim/Documents/libraries/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/nayeongkim/Documents/study/flutter_inflean_basic/stateful_widget"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/nayeongkim/Documents/study/flutter_inflean_basic/stateful_widget/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/nayeongkim/Documents/study/flutter_inflean_basic/stateful_widget/.dart_tool/package_config.json"
